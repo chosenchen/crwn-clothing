688f4ae749e82dfa9bf4b157a661c327d8ef5d35
Chaoran Chen <chosenchen@outlook.com>
2022-02-14 18:38:07 -0800

    implemented firebase utils

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -11,6 +11,32 @@ const config = {
     appId: "1:387851994999:web:460462725eafcf95180372"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
