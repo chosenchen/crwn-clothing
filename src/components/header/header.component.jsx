ebfd45d10b542ba77b0c7dd3d08b896cfb9a4915
Chaoran Chen <chosenchen@outlook.com>
2022-02-24 15:23:03 -0800

    implement redux in header cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { ReactComponent as Logo} from '../assets/crown.svg';
+import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.util';
-
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link to='/' className='logo-container'>
             <Logo className="log"></Logo>
@@ -17,12 +18,18 @@ const Header = ({currentUser}) => (
             <Link className="option" to='/shop'>CONTACT</Link>
             {currentUser? 
             <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>: <Link className="option" to='/signin'>SIGN IN</Link>}
+            <CartIcon/>
         </div>
+        {
+            hidden? null: <CartDropdown/>
+        }
+
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}} )=> ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
