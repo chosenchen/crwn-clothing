76dc17b75280cc01047f34297a9bcf318a6c8bff
Chaoran Chen <chosenchen@outlook.com>
2022-02-24 14:43:07 -0800

    user Redirect and user action type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -43,7 +43,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route exact path='/shop' component={ShopPage}></Route>
-          <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
+          <Route exact path='/signin' render={()=> this.props.currentUser? (<Redirect to="/" />):(<SignInAndSignUpPage/>)}></Route>
         </Switch>
       </div>
     );
@@ -51,8 +51,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps =({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps =dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/redux/user/user.types.js
@@ -0,0 +1,3 @@
+export const UserActionTypes = {
+    Set_CURRENT_USER: 'SET_CURRENT_USER'
+}
